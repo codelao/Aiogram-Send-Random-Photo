31fc6fd580e9df15bc6b10fd18960ec091920a86
ArsezSE <arsezsoftware@gmail.com>
2022-12-27 22:52:59 +0300

    Aiogram-Random-Photo-Generator

---
+++ bot.py
@@ -0,0 +1,23 @@
+import logging
+import markups as nav
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN
+
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    if message.chat.type == 'private':
+        await bot.send_message(message.chat.id, "Hi!", reply_markup=nav.Menu)
+
+@dp.callback_query_handler(text='GetPhoto')
+async def getphoto(message: types.Message):
+    await bot.send_photo(message.from_user.id, photo=types.InputFile.from_url("https://bing.ioliu.cn/v1/rand"))
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
---
+++ markups.py
@@ -0,0 +1,6 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+Menu = InlineKeyboardMarkup(row_width=1)
+btnGetPhoto = InlineKeyboardButton(text="📍Get random photo from Bing", callback_data='GetPhoto')
+Menu.add(btnGetPhoto)
