c5cc9b2c3e84b086fc913211402b01a4c3747b3b
Code Lao <109976097+codelao@users.noreply.github.com>
2023-03-06 23:38:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,12 +9,12 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await bot.send_message(message.chat.id, "Hi!", reply_markup=nav.Menu)
+    await bot.send_message(message.chat.id, 'Hi!', reply_markup=nav.Menu)
 
       
 @dp.callback_query_handler(text='GetPhoto')
 async def getphoto(message: types.Message):
-    await bot.send_photo(message.from_user.id, photo=types.InputFile.from_url("https://bing.ioliu.cn/v1/rand"))
+    await bot.send_photo(message.from_user.id, photo=types.InputFile.from_url('https://bing.ioliu.cn/v1/rand'))
 
 
 if __name__ == "__main__":
