4ff35f42d90248014ee1c10054331dc82abb33be
Code Lao <109976097+codelao@users.noreply.github.com>
2023-03-06 23:39:33 +0300

    Update markups.py

--- markups.py
+++ markups.py
@@ -2,5 +2,5 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 Menu = InlineKeyboardMarkup(row_width=1)
-btnGetPhoto = InlineKeyboardButton(text="📍Get random photo from Bing", callback_data='GetPhoto')
+btnGetPhoto = InlineKeyboardButton(text='📍Get random photo from Bing', callback_data='GetPhoto')
 Menu.add(btnGetPhoto)
